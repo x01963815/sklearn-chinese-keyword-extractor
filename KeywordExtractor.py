b27a3566bcce71ffcac22e7c1a81895f204ad25d
Ferris Liu <x01963815@gmail.com>
2020-03-19 10:01:59 +0800

    add function docstrings

--- KeywordExtractor.py
+++ KeywordExtractor.py
@@ -14,14 +14,55 @@ from sklearn.utils.validation import check_is_fitted, check_array
 
 
 class KeywordExtractor(BaseEstimator, TransformerMixin):
+    ''' Chinese Keyword Extractor
+    
+    The model will process a brute-force search algorithm to find the 
+    most repeated substrings in a string. The substrings are defined as
+    keywords. The frequency of a keyword will be calibrated by its 
+    parental string.
+
+
+    Parameters
+    ----------
+    n_keyword: int, default=5
+        Number of keywords to keep.
+        If number of extracted keywords is less than n_keyword, model 
+        will pad with "" string to make sure the same shape.
+    
+    min_ch_keyword_len: int, default=2
+        Filter the min length of Chinese keyword. Model would drop the 
+        keyword with length less that min_ch_keyword_len.
+    
+    min_en_keyword_len: int, default=3
+        Filter the min length of English keyword. Model would drop the 
+        keyword with length less that min_en_keyword_len.
+    
+    ngram_range: tuple of int, default=(0,10)
+        CountVectorizer parameter. The value of min_ch_keyword_len and
+        min_en_keyword_len should be in ngram_range.
+    
+    enable_english: bool, default=True
+        If False, the model would filter out English keywords, and 
+        min_en_keyword_len and to_lowercase parameter would be 
+        meaningless.
+    
+    to_lowercase: bool, default=False
+        If True, the model would convert English string to lower case.
+        It might extract different keywords.
+    
+    Attributes
+    ----------
+    keywords_components_: OrderedDict
+        
+    '''
     def __init__(self, 
-                 n_keyword=5, ngram_range=(0,10),
+                 n_keyword=5, 
+                 ngram_range=(0,10),
                  min_ch_keyword_len=2, 
                  min_en_keyword_len=3,
                  enable_english=True,
                  to_lowercase=False):
-        '''
-        '''
+
         self.n_keyword = n_keyword
         self.min_ch_keyword_len = min_ch_keyword_len
         self.min_en_keyword_len = min_en_keyword_len
@@ -31,7 +72,20 @@ class KeywordExtractor(BaseEstimator, TransformerMixin):
         
     
     def fit(self, X, y=None):
-        '''
+        '''Fit the model with X.
+        
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, ), List of string
+            One dimension array or list of names
+        
+        y : None
+            Ignored variable.
+            
+        Returns
+        -------
+        self: object
+            Returns the instance itself.
         '''
         
         corpus = list(map(lambda s: ''.join(self._split_string(s)),
@@ -52,6 +106,22 @@ class KeywordExtractor(BaseEstimator, TransformerMixin):
         return self
     
     def transform(self, X):
+        '''Extract keywords from X.
+        
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, ), List of string
+            One dimension array or list of names
+            
+        
+        Returns
+        -------
+        labels: array-like, shape (n_samples, n_keyword)
+            Return keyword strings extracted from sample.
+            If the number of extraced keywords is less than n_keyword,
+            model will pad with "" string.
+        
+        '''
         
         # Check is fit had been called
         check_is_fitted(self, ['keywords_components_'])
@@ -68,7 +138,22 @@ class KeywordExtractor(BaseEstimator, TransformerMixin):
         return labels
     
     def transform_proba(self, X):
+        '''Extract gain value of keywords from X.
         
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, ), List of string
+            One dimension array or list of names
+            
+        
+        Returns
+        -------
+        gainvalues: array-like, shape (n_samples, n_keyword)
+            Return gain value(int) of keywords extracted from sample.
+            If the number of extraced keywords is less than n_keyword,
+            model will pad with 0.
+        
+        '''
         # Check is fit had been called
         check_is_fitted(self, ['keywords_components_'])
         
@@ -85,11 +170,30 @@ class KeywordExtractor(BaseEstimator, TransformerMixin):
     
     
     def fit_transform(self, X, y=None):
+        '''Fit the model with X and extract keywords from X.
+        
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, ), List of string
+            One dimension array or list of names
+            
+        y : None
+            Ignored variable.
+            
+        Returns
+        -------
+        labels: array-like, shape (n_samples, n_keyword)
+            Return keyword strings extracted from sample.
+            If the number of extraced keywords is less than n_keyword,
+            model will pad with "" string.
+        
+        '''
         self.fit(X) 
         return self.transform(X)
     
     
     def _modify_string(self, string):
+        '''Apply strip string and lowercases  '''
         modified_string = string
         
         if self.to_lowercase:
@@ -99,7 +203,8 @@ class KeywordExtractor(BaseEstimator, TransformerMixin):
         return modified_string
     
     
-    def _split_string(self, string):       
+    def _split_string(self, string):  
+        '''Split sample name into list of strings'''
         string = self._modify_string(string)
         
         if not string:
@@ -114,7 +219,7 @@ class KeywordExtractor(BaseEstimator, TransformerMixin):
         
     
     def _parse_en_substring(self, substring):
-        
+        '''Parse English string to get keyword and gain value'''
         if len(substring) >= self.min_en_keyword_len:
             keywords_fltr = np.array([substring])
             gain_value_fltr = np.array([self.keywords_components_
@@ -127,7 +232,7 @@ class KeywordExtractor(BaseEstimator, TransformerMixin):
     
     
     def _parse_ch_substring(self, substring):
-        
+        '''Parse Chinese string to get keywords and gain values'''
         # Pascal Structure
         len_str = max(len(substring), 1) # min 1, prevent empty string
         start_position = np.tril(np.indices((len_str,len_str))[1])    
@@ -170,7 +275,7 @@ class KeywordExtractor(BaseEstimator, TransformerMixin):
     
     
     def _parse_string(self, string):
-        
+        '''Parse sample string'''
         substring_list = self._split_string(string)
         
         keywords = np.array([], dtype='U32')
@@ -184,8 +289,7 @@ class KeywordExtractor(BaseEstimator, TransformerMixin):
                 keyword, gain_value = self._parse_ch_substring(substr)
             
             keywords = np.concatenate([keywords, keyword])
-            gain_values = np.concatenate([gain_values, gain_value])
-            
+            gain_values = np.concatenate([gain_values, gain_value])   
         
         # Sort by gain_value
         dtype = [('keyword', 'U32'), ('gain_value', int)]
